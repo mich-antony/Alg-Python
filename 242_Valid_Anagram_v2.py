002b1d02b3a8e54cbd803518a845a132bed1bd51
mich-antony <mich_antony@yahoo.com>
2022-04-15 12:06:29 -0400

    Create 242_Valid_Anagram_v2.py

---
+++ 242_Valid_Anagram_v2.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) != len(t):
+            return False
+        
+        strhash = {}
+        
+        for i in range(len(s)):
+            strhash[s[i]] = strhash.get(s[i], 0) + 1            
+            strhash[t[i]] = strhash.get(t[i], 0) - 1
+        
+        for val in strhash:
+            if strhash[val] != 0:
+                return False
+        return True
